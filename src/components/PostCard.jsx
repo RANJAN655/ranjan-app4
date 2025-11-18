54b6cd0667bd7afb5773e88c78fe3f569b9f5d57
RANJAN655 <ranjankumar976666@gmail.com>
2025-11-18 16:27:28 +0530

    add all

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,7 +3,7 @@ import appwriteService from "../appwrite/config";
 import { Link } from "react-router-dom";
 
 function PostCard({ $id, title, featuredImage, }) {
-  const previewUrl = appwriteService.getFilePreview(featuredImage); // full file URL
+  const previewUrl = appwriteService.getFileDownload(featuredImage); // full file URL
   // append mode=admin if needed
   const imageUrl = previewUrl.includes("?")
     ? previewUrl + "&mode=admin"
